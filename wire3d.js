bd74cbf49d917f75e9679462613ec1f61013fb4a
Ali Mihandoost <ali@mihandoost.com>
2012-05-18 10:50:30 +0430

    Base Structure and design pattern
    I Love Zepto Design Pattern :D

--- wire3d.js
+++ wire3d.js
@@ -4,10 +4,61 @@
 	by Ali Mihandoost - i@ali.md
 */
 
-(function(_root){
+(function(window,undefined){
 
-	_root.wire3d = function(canvas){
+	!!Object.prototype.extend || (Object.prototype.extend = function(obj) {
+		for(i in obj) this[i]=obj[i]; return this;
+	});
+
+	window.wire3d = function(canvas){
+
+		//variables
+		var points =[], faces = [];
+
+		//core funcs
+		function log (){
+			console.log({
+				points:points,
+				faces:faces
+			});
+		} this.log = log;
+
+		function addPoint (p3d) {
+			points.push({x:0,y:0,z:0}.extend(p3d)); return this;
+		} this.addPoint=addPoint;
+
+		function addPoints (arr) {
+			arr.forEach(function(point){
+				addPoint(point);
+			}); return this;
+		} this.addPoints=addPoints;
+
+		function addFace (points, style) {
+			faces.push({
+				points : points,
+				style  : {
+					faceColor : 'rgba(50,50,200,0.7)',
+					borderWidth : 1,
+					borderColor : 'rgb(0,0,0,0.9)'
+				}.extend(style)
+			}); return this;
+		} this.addFace = addFace;
+
+		function getVisible (p) {
+			return ((p[1].x-p[0].x)*(p[2].y-p[0].y)<(p[2].x-p[0].x)*(p[1].y-p[0].y));
+		}
+
+		function drawFace (face){
+
+		}
+
+		this.render = function () {
+			faces.forEach(function (face) {
+				drawFace(face);
+			}); return this;
+		}
 
 	}
 
-})(window)
+})(window);
+
